4ca115ea237ec5396a5f7c727e5718938623eeab
mrkov <60403788+mrkov@users.noreply.github.com>
2020-05-20 14:10:22 +0200

    added shop page, collection preview, collection item...

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import HomePage from './pages/homepage/HomePage';
 import { Route } from 'react-router-dom';
 import HatsPage from './pages/hatspage/HatsPage';
+import ShopPage from './pages/ShopPage/ShopPage';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       
       <Route exact path='/' component={HomePage} />
       <Route path='/shop/hats' component={HatsPage} />
+      <Route exact path='/shop' component={ShopPage} />
     </div>
   );
 }
---
+++ src/pages/ShopPage/ShopPage.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import SHOP_DATA from './2.2 shop.data.js.js'
+import CollectionPreview from '../../components/collection-preview/CollectionPreview.js'
+
+export default class ShopPage extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             collections: SHOP_DATA
+        }
+    }
+
+    componentDidMount(){
+        console.log(this.state.collections)
+    }
+    
+    render() {
+        const {collections} = this.state;
+        return (
+            <div className='shop-page'>
+                <h1>SHOP PAGE COLLECTIONS</h1>
+                {collections.map(({id, ...otherCollectionProps}) => 
+                    <CollectionPreview key={id} {...otherCollectionProps} />
+                )}
+            </div>
+        )
+    }
+}
